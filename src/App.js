7365c39ba417b1d158a2b4f157c2bbcfead79064
vadserg <vadserg@list.ru>
2022-06-20 01:36:32 +0300

    mockapi.io

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import Card from './components/Card';
+import React from 'react';
+import Card from './components/Card/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
 function App() {
+	const [cartOpened, setCartOpened] = React.useState(false);
+	const [items, setItems] = React.useState([]);
+	const [cartItems, setCartItems] = React.useState([]);
+	const [favoritItems, setFavoritItems] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch('https://62af03f03bbf46a3521a4c67.mockapi.io/items')
+			.then(res => res.json())
+			.then(data => {
+				setItems(data);
+			});
+	}, []);
+
+	const addToCart = good => {
+		if (cartItems.find(item => item.id === good.id)) {
+			let index = cartItems.findIndex(item => item.id === good.id);
+			cartItems.splice(index, 1);
+			setCartItems(cartItems);
+		} else {
+			setCartItems(prev => [...prev, good]);
+		}
+
+		console.log(cartItems);
+	};
+
+	const addToFavorit = good => {
+		if (favoritItems.find(item => item.id === good.id)) {
+			const index = favoritItems.findIndex(item => item.id === good.id);
+			favoritItems.splice(index, 1);
+		} else {
+			favoritItems.push(good);
+		}
+
+		setFavoritItems(favoritItems);
+		//console.log(favoritItems);
+	};
+
+	const deleteCartItem = id => {
+		const index = cartItems.findIndex(item => item.id === id);
+		cartItems.splice(index, 1);
+		setCartItems(cartItems);
+		console.log(cartItems);
+	};
+
 	return (
 		<div className='wrapper'>
-			<Drawer display='none' />
-			<Header />
+			{cartOpened && (
+				<Drawer
+					items={cartItems}
+					onClose={() => setCartOpened(false)}
+					onDeleteItem={obj => deleteCartItem(obj)}
+				/>
+			)}
+			<Header onClickCart={() => setCartOpened(true)} />
 
 			<div className='content'>
 				<div className='contentInfo'>
@@ -17,44 +68,15 @@ function App() {
 					</div>
 				</div>
 
-				<div className='cardsRow'>
-					<Card />
-
-					<div className='card'>
-						<div className='favorite'>
-							<img src='img/unliked.svg' alt='like' />
-						</div>
-						<img src='img/sneakers/2.jpg' alt='img' width={133} height={112} />
-						<p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-						<div className='cardBottom'>
-							<div className='cardPrice'>
-								<span>Цена:</span>
-								<b>12 999 руб.</b>
-							</div>
-							<button className='btnAdd'>
-								<img src='/img/plus.svg' alt='plus' width={11} height={11} />
-							</button>
-						</div>
-					</div>
-
-					<Card />
-
-					<div className='card'>
-						<div className='favorite'>
-							<img src='img/unliked.svg' alt='like' />
-						</div>
-						<img src='img/sneakers/4.jpg' alt='img' width={133} height={112} />
-						<p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-						<div className='cardBottom'>
-							<div className='cardPrice'>
-								<span>Цена:</span>
-								<b>12 999 руб.</b>
-							</div>
-							<button className='btnAdd'>
-								<img src='/img/plus.svg' alt='plus' width={11} height={11} />
-							</button>
-						</div>
-					</div>
+				<div className='cards'>
+					{items.map(item => (
+						<Card
+							key={item.id}
+							{...item}
+							onPlus={obj => addToCart(obj)}
+							onFavorit={obj => addToFavorit(obj)}
+						/>
+					))}
 				</div>
 				{/* end of cardsRow */}
 			</div>
